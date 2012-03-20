bfb6376fc49dcfe0488c901143af406a6aaac7d2
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-03-20 20:39:20 +0100

    Instacia del ClassLoader

---
+++ app/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__.'/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
