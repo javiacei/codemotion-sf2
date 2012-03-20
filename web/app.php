1e9dabffcb6d664ad46f466b4bf999d0642d302d
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-03-20 21:16:02 +0100

    Utilización de outbuffering y echo del contenido

--- web/app.php
+++ web/app.php
@@ -17,11 +17,17 @@ if ($name = $request->get('name')) {
     $tasks = $taskManager->getAll();
 }
 
+/* Utilizamos Output Buffering para no enviar el contenido de la vista y lo recogemos
+ * en la variable $content
+ */
+ob_start();
+include __DIR__ . '/../src/Codemotion/View/Task/list.php';
+$content = ob_get_clean();
+
 /* Enviamos la cabecera HTTP con estado OK (200) */
 header('HTTP 1.0 200 OK');
 header('Content-Type: text/html UTF-8');
 $now = new \DateTime();
 header(sprintf('date: /%s ', $now->format("Y-m-d H:i:s")));
 
-/* Enviamos el contenido HTTP */
-include __DIR__ . '/../src/Codemotion/View/Task/list.php';
+echo $content;
