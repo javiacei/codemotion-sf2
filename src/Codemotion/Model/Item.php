6d2110b1f6c4e130e3509ca1daa82a69027b32b4
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-03-21 14:24:56 +0100

    Task e Item con setters y getter

--- src/Codemotion/Model/Item.php
+++ src/Codemotion/Model/Item.php
@@ -24,4 +24,58 @@ class Item
      * @JoinColumn(name="task_id", referencedColumnName="id")
      */
     protected $task;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Item
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set task
+     *
+     * @param Codemotion\Model\Task $task
+     * @return Item
+     */
+    public function setTask(\Codemotion\Model\Task $task = null)
+    {
+        $this->task = $task;
+        return $this;
+    }
+
+    /**
+     * Get task
+     *
+     * @return Codemotion\Model\Task 
+     */
+    public function getTask()
+    {
+        return $this->task;
+    }
 }
--- src/Codemotion/Model/Task.php
+++ src/Codemotion/Model/Task.php
@@ -28,4 +28,85 @@ class Task
      * @OneToMany(targetEntity="Item", mappedBy="task", cascade={"all"})
      */
     protected $items;
+
+    public function __construct()
+    {
+        $this->items = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     * @return Task
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Add items
+     *
+     * @param Codemotion\Model\Item $items
+     * @return Task
+     */
+    public function addItem(\Codemotion\Model\Item $items)
+    {
+        $this->items[] = $items;
+        return $this;
+    }
+
+    /**
+     * Get items
+     *
+     * @return Doctrine\Common\Collections\Collection
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
 }
