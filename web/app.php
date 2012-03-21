6e9541a9ddd5de02843dc09729c1f9a0fc1d2e6d
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-03-21 20:56:34 +0100

    Eliminamos database.php y pasamos dependencia de entity_manager al controlador

--- web/app.php
+++ web/app.php
@@ -1,17 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../app/bootstrap.php';
-require_once __DIR__ . '/../app/database.php';
-
-use Codemotion\Controller\TaskController;
-
-use Symfony\Component\HttpFoundation\Request;
-
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-use Symfony\Component\Routing\Router;
-
-use Symfony\Component\Config\FileLocator;
 
 $request = $container->get('request');
 
@@ -22,7 +11,7 @@ $controller = new $parameters['controller'];
 $action     = $parameters['action'];
 
 /* Dependencias del controlador */
-$controller->setEntityManager($em);
+$controller->setEntityManager($container->get('doctrine.entity_manager'));
 $controller->setTemplating($container->get('twig'));
 $controller->setRouting($router);
 
