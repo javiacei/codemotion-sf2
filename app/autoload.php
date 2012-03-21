2c9266d56557b322a62f31b085ef9f3275abfd43
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2012-03-21 15:06:44 +0100

    Instalación y carga de Twig

--- app/autoload.php
+++ app/autoload.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/Symfony/Component/ClassLoader/UniversalClassLoa
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
+// PSR-0
 $loader->registerNamespaces(array(
     'Codemotion' => __DIR__ . '/../src',
     'Symfony' => __DIR__ . '/../vendor',
@@ -13,5 +14,10 @@ $loader->registerNamespaces(array(
         __DIR__ . '/../vendor/doctrine-dbal/lib',
         __DIR__ . '/../vendor/doctrine/lib'
     )
+));
+
+// PEAR
+$loader->registerPrefixes(array(
+    'Twig_' => __DIR__ . '/../vendor/twig/lib'
 ));
 $loader->register();
